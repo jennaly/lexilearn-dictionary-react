41480424b3283c42778340a8a21cf4005808d254
jennaly <106183040+jennaly@users.noreply.github.com>
2023-01-06 20:16:08 -0800

    add favorite word toggle

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Illustration from './Illustration';
 import CardBody from './CardBody';
 
-const Card = ({ wordData, cardTitle, setFavoriteWords }) => {
+const Card = ({ wordData, cardTitle, favoriteWords, setFavoriteWords }) => {
+
+    const [isFavorite, setIsFavorite] = useState(false);
+
+    const toggleFavorite = () => {
+        setIsFavorite(prevIsFavorite => !prevIsFavorite);
+
+        if (!isFavorite) {
+            addWordToFavorites();
+        } else {
+            removeWordFromFavorites();
+        }
+    }
   
     const addWordToFavorites = () => {
         const definitions = wordData.map(e => e.definition);
@@ -17,6 +29,12 @@ const Card = ({ wordData, cardTitle, setFavoriteWords }) => {
         setFavoriteWords(prevFavoriteWords => [...prevFavoriteWords, entry])
     }
 
+    const removeWordFromFavorites = () => {
+        const newFavoritesList = favoriteWords.filter(word => word.term !== cardTitle);
+
+        setFavoriteWords(newFavoritesList);
+    }
+    
     return (
         <div>
             <div className="card lg:card-side bg-base-100 shadow-xl">
@@ -36,10 +54,12 @@ const Card = ({ wordData, cardTitle, setFavoriteWords }) => {
                     
                     {wordData.length > 0 && 
                         <button 
-                        className="btn btn-primary"
-                        onClick={addWordToFavorites}
+                        className={`btn ${isFavorite ? "" : "btn-outline"} gap-2`}
+                        
+                        onClick={toggleFavorite}
                         >
-                            Add to Favorites
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
+                            Favorite
                         </button>
                     }
 
