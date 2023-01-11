835df7fb1127fe4607634668426064ae3264c6f3
jennaly <106183040+jennaly@users.noreply.github.com>
2023-01-11 09:37:11 -0800

    style favorite words component

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -10,10 +10,11 @@ const Favorites = ({ favoriteWords, getWordData, setWordData, setCardTitle }) =>
     }
 
     return (
-        <div>
+        <div className="max-w-xl mx-auto flex flex-wrap gap-8 justify-center mt-10">
             { favoriteWords.length > 0 && favoriteWords.map((word, index) => {
                 return <span 
-                        key={index} 
+                        key={index}
+                        className="mx-4 py-4 px-8 font-gaegu text-yellow-700 text-xl sticky-note" 
                         onClick={() => handleSubmit(favoriteWords[index].term)}
                         >
                             {word.term}
