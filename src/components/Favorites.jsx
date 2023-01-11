36017a70289003b653f5d101768de10eca7d7a5b
jennaly <106183040+jennaly@users.noreply.github.com>
2023-01-11 10:21:27 -0800

    add remove from favorites button on each favorite word

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Favorites = ({ favoriteWords, getWordData, setWordData, setCardTitle }) => {
+const Favorites = ({ favoriteWords, setFavoriteWords, getWordData, setWordData, setCardTitle }) => {
       
     const handleSubmit =  async (word) => {
         const data = await getWordData(word);
@@ -9,16 +9,29 @@ const Favorites = ({ favoriteWords, getWordData, setWordData, setCardTitle }) =>
         setCardTitle(data.word);
     }
 
+    const removeWordFromFavorites = (word) => {
+        const newFavoritesList = favoriteWords.filter(favoriteWord => favoriteWord.term !== word);
+
+        setFavoriteWords(newFavoritesList);
+    }
+
     return (
         <div className="max-w-xl mx-auto flex flex-wrap gap-8 justify-center mt-10">
             { favoriteWords.length > 0 && favoriteWords.map((word, index) => {
-                return <span 
+                return <div 
                         key={index}
-                        className="mx-4 py-4 px-8 font-gaegu text-yellow-700 text-xl sticky-note" 
                         onClick={() => handleSubmit(favoriteWords[index].term)}
+                        className="inline-block mx-4 py-4 px-8 font-gaegu text-yellow-700 text-xl sticky-note relative" 
                         >
-                            {word.term}
-                        </span>
+                            <span className="p-2">{word.term}</span>
+                            <div className="absolute -right-2 -top-2">
+                                <button 
+                                onClick={() => removeWordFromFavorites(favoriteWords[index].term)}
+                                className="rounded-full p-2 hover:borde hover:border-yellow-800">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                                </button>
+                            </div>
+                        </div>
             })}
         </div>
     )
