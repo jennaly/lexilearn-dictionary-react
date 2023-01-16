8ffb7d062bbaa9596e9ceb23e07eeab425104230
jennaly <106183040+jennaly@users.noreply.github.com>
2023-01-15 16:05:18 -0800

    move placement of authcontext and wordcontext provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { WordContextProvider } from './context/WordContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <WordContextProvider>
+        <App />
+      </WordContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>,
 )
