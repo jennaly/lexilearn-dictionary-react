4dd405176f8557b7fad0eb70f3f15e39e1092df7
jennaly <106183040+jennaly@users.noreply.github.com>
2023-01-15 20:46:25 -0800

    navbar init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
+import Title from './Title';
+
+const Navbar = () => {
+    const { logout } = useLogout();
+
+    const handleClick = () => {
+        logout();
+    }
+
+    return (
+        <header>
+            <div>
+                <nav>
+                    <div>
+                        <button onClick={handleClick}>Log out</button>
+                    </div>
+                    <div>
+                        <Link to="/login">Login</Link>
+                        <Link to="/signup">Signup</Link>
+                    </div>
+                </nav>
+                <Link to="/"><Title /></Link>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
