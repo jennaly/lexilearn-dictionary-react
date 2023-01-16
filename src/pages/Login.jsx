bd778b1cd02d739493f5e12f6c9fd89a4edfd2f7
jennaly <106183040+jennaly@users.noreply.github.com>
2023-01-15 21:17:00 -0800

    create useLogin hook

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react';
+import { useLogin } from '../hooks/useLogin';
 import Title from '../components/Title';
+
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const { login, error, isLoading } = useLogin();
 
-    const handleSubmit = async () => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        console.log(email, password);
+        await login(email, password);
     }
     return (
         <div>
@@ -31,7 +34,11 @@ const Login = () => {
                 value={password}
                 />
 
-                <button>Login</button>
+                <button disabled={isLoading}>Login</button>
+
+                { error && 
+                    <div>{error}</div>
+                }
             </form>
         </div>
     )
