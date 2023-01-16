ab2b64763c4e541cee50b258a30266b4d32dead8
jennaly <106183040+jennaly@users.noreply.github.com>
2023-01-15 22:03:21 -0800

    nest routes in layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Layout from './components/Layout';
 
@@ -6,33 +6,27 @@ import Index from './pages/Index';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
-import { useAuthContext } from "./hooks/useAuthContext";
-
 function App() {
-  const { user } = useAuthContext();
-
   return (
     <BrowserRouter>
-      <Routes>
-        {!user && 
+      <Layout>
+        <Routes>
           <Route 
           path="/login"
           element={ <Login />}
           />
-        }  
-
-        {!user && 
+      
           <Route 
           path="/signup"
           element={<Signup />}
-        />
-        }  
-
-        <Route 
-          path="/"
-          element={<Layout><Index /></Layout>}
-        />
-      </Routes>
+          />
+        
+          <Route 
+            path="/"
+            element={<Index />}
+          />
+       </Routes>
+      </Layout>
     </BrowserRouter>
   )
 }
