b3a9fd0f9dcd54424997e672e733841fd7372463
jennaly <106183040+jennaly@users.noreply.github.com>
2023-01-16 21:48:17 -0800

    add authorization to requests to db

--- src/context/WordContext.jsx
+++ src/context/WordContext.jsx
@@ -1,86 +1,98 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const WordContext = createContext();
 
 export const WordContextProvider = ({ children }) => {
 
-    // temporarily not needed for the purpose of testing crud operations to mongodb
-    const savedFavoriteWords = JSON.parse(localStorage.getItem('favoriteWords'));
+  const [searchWord, setSearchWord] = useState("");
+  const [wordData, setWordData] = useState([]);
+  const [wordDifficulty, setWordDifficulty] = useState(0);
+  const [cardTitle, setCardTitle] = useState("");
+  const [favoriteWords, setFavoriteWords] = useState([]);
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const [searchWord, setSearchWord] = useState("");
-    const [wordData, setWordData] = useState([]);
-    const [wordDifficulty, setWordDifficulty] = useState(0);
-    const [cardTitle, setCardTitle] = useState("");
-    const [favoriteWords, setFavoriteWords] = useState([]);
-    const [isFavorite, setIsFavorite] = useState(false);
-    const [loading, setLoading] = useState(false);
+  const { user } = useAuthContext();
 
-    // temporary state until login/auth is set up
-    const [user, setUser] = useState(true);
-  
-    useEffect(() => {
-      const getWordsFromDB = async () => {
-        const res = await fetch (
-          `http://localhost:8882/api/favoriteWords`
-        );
-        const data = await res.json();
-        setFavoriteWords(data);
-      }
-      getWordsFromDB();
-    }, []);
-  
-    useEffect(() => {
-      const checkIsFavorite = () => {
-        for (const word of favoriteWords) {
-          if (word.term === cardTitle) {
-            setIsFavorite(true);
-            return;
-          } 
+  useEffect(() => {
+    const fetchWords = async () => {
+      const res = await fetch( `http://localhost:8882/api/favoriteWords`, {
+        headers: {
+          'Authorization': `Bearer ${user.token}`
         }
-        setIsFavorite(false);
-      }
-      checkIsFavorite();
-    }, [wordData, favoriteWords]);
-  
-    useEffect(() => {
-      localStorage.setItem('favoriteWords', JSON.stringify(favoriteWords));
-    }, [favoriteWords]);
-  
-    const getWordData = async (word) => {
-      const res = await fetch (
-          `https://lexilearn-proxy-api.cyclic.app/api/dictionary/${word}`
-        )
-  
-      const data = await res.json();
-  
-      return data; 
-  
-    }
+      });
 
-    return (
-        <WordContext.Provider
-            value={{
-                user,
-                loading,
-                setLoading,
-                wordData,
-                setWordData,
-                getWordData,
-                wordDifficulty,
-                setWordDifficulty,
-                searchWord,
-                setSearchWord,
-                cardTitle,
-                setCardTitle,
-                favoriteWords,
-                setFavoriteWords,
-                isFavorite,
-                setIsFavorite,
-            }}
-        >
-            {children}
-        </WordContext.Provider>
-    )
+      const data = await res.json();
+
+      setFavoriteWords(data);
+    };
+
+    const getWordsLocally = () => {
+      const words = JSON.parse(localStorage.getItem('favoriteWords'));
+
+      setFavoriteWords(words);
+    };
+
+    if (user) {
+      fetchWords();
+    } else {
+      getWordsLocally();
+    }
+  }, [user]);
+
+  useEffect(() => {
+    const checkIsFavorite = () => {
+      for (const word of favoriteWords) {
+        if (word.term === cardTitle) {
+          setIsFavorite(true);
+          return;
+        } 
+      }
+      setIsFavorite(false);
+    }
+    checkIsFavorite();
+  }, [wordData, favoriteWords]);
+
+  useEffect(() => {
+    localStorage.setItem('favoriteWords', JSON.stringify(favoriteWords));
+  }, [favoriteWords]);
+
+  const getWordData = async (word) => {
+    const res = await fetch (
+        `https://lexilearn-proxy-api.cyclic.app/api/dictionary/${word}`
+      )
+
+    const data = await res.json();
+
+    return data; 
+
+  }
+
+  return (
+      <WordContext.Provider
+          value={{
+              user,
+              loading,
+              setLoading,
+              wordData,
+              setWordData,
+              getWordData,
+              wordDifficulty,
+              setWordDifficulty,
+              searchWord,
+              setSearchWord,
+              cardTitle,
+              setCardTitle,
+              favoriteWords,
+              setFavoriteWords,
+              isFavorite,
+              setIsFavorite,
+          }}
+      >
+          {children}
+      </WordContext.Provider>
+  )
 
 }  
 
