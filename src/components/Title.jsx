c27632d2a80318911ad2a7ac5ff42c4578bf9d36
jennaly <106183040+jennaly@users.noreply.github.com>
2023-01-17 11:39:14 -0800

    style navbar

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,15 +3,11 @@ import { Link } from 'react-router-dom';
 
 const Title = () => {
     return (
-        <div>
-            <div className="pt-5 inline-block flex justify-center">
-                    <Link to="/">
-                        <h1 className="font-fredoka-one font-semibold uppercase text-yellow-700 tracking-widest text-5xl lg:text-7xl text-center text-yellow-700 hover:text-yellow-800">
-                        Lexi &bull; Learn
-                        </h1>
-                    </Link>
-            </div>
-        </div>
+        <Link to="/">
+            <h1 className="font-fredoka-one font-semibold uppercase text-yellow-700 tracking-widest text-5xl lg:text-7xl text-center text-yellow-700 hover:text-yellow-800">
+            Lexi &bull; Learn
+            </h1>
+        </Link>
     )
 }
 
