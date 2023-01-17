d1545801b068cc85a567951aea2db4c469233be3
jennaly <106183040+jennaly@users.noreply.github.com>
2023-01-17 12:23:12 -0800

    style login and signup pages

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -12,30 +12,42 @@ const Signup = () => {
         await signup(email, password);
     }
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <h2>Sign Up</h2>
+        <div className="mb-4 mt-6 lg:max-w-xl mx-auto">
+            <div className="card lg:w-96 bg-base-100 shadow-xl lg:h-[375px] flex flex-col">
+                <form onSubmit={handleSubmit} className="card-body justify-between text-lg lg:text-xl">
+                    <h2 className="card-title font-fredoka-one text-4xl lg:text-5xl underline-offset-14 w-full pb-1 lg:pb-4 mr-6 border-b-2 border-yellow-700 text-yellow-700">
+                        Signup
+                    </h2>
 
-                <label>Email:</label>
-                <input
-                type="email"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                />
+                    <div className="mt-4 font-gaegu uppercase flex flex-col text-yellow-700">
+                        <label className="font-bold p-0.5">Email:</label>
+                        <input
+                        type="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="example@gmail.com"
+                        value={email}
+                        className="p-0.5 mb-1"
+                        />
 
-                <label>Password:</label>
-                <input
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-                />
+                        <label className="p-0.5">Password:</label>
+                        <input
+                        type="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="your password"
+                        value={password}
+                        className="p-0.5"
+                        />
+                    </div>
 
-                <button disabled={isLoading}>Sign up</button>
+                    <button disabled={isLoading} className="bg-yellow-700 font-gaegu m-1 rounded-md w-full tracking-widest hover:bg-yellow-800 text-base-100 text-lg lg:text-xl ">
+                        Signup
+                    </button>
 
-                { error && 
-                    <div>{error}</div>
-                }
-            </form>
+                    { error && 
+                        <div className="font-gaegu text-yellow-500 text-center">{error}</div>
+                    }
+                </form>
+            </div>
         </div>
     )
 }
