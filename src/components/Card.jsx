7a44f251d363834d083c64c3a71a602b4e2172d9
jennaly <106183040+jennaly@users.noreply.github.com>
2023-01-26 12:34:07 -0800

    remove unnecessary containers

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -106,63 +106,56 @@ const Card = ({ showCard }) => {
     }
 
     return (
-            <div className="max-w-sm mb-4 lg:max-w-xl mx-auto">
+            <div className="max-w-sm mb-4 lg:max-w-xl mx-auto card bg-base-100 shadow-xl lg:h-[375px] p-5">
                 
-                <div className="card bg-base-100 shadow-xl lg:h-[375px]">
+                <div className="flex flex-col lg:flex-row justify-between items-center">
+                    {wordData.definitions &&
+                        <div className="card-title font-fredoka-one text-4xl lg:text-5xl underline-offset-14 w-full pb-1 lg:pb-4 lg:mr-6 border-b-2 border-yellow-700 text-yellow-700">
 
-                    <div className="p-5">
+                            <div className="w-full flex gap-2 ">
+                                <h2>{wordData.word}</h2>
 
-                        <div className="flex flex-col lg:flex-row justify-between items-center">
-                            {wordData.definitions &&
-                                <div className="card-title font-fredoka-one text-4xl lg:text-5xl underline-offset-14 w-full pb-1 lg:pb-4 lg:mr-6 border-b-2 border-yellow-700 text-yellow-700">
+                                {wordData.definitions && wordData.definitions[0].emoji && 
+                                    <span>{wordData.definitions[0].emoji}</span>
+                                }
+                            </div>
 
-                                    <div className="w-full flex gap-2 ">
-                                        <h2>{wordData.word}</h2>
-
-                                        {wordData.definitions && wordData.definitions[0].emoji && 
-                                            <span>{wordData.definitions[0].emoji}</span>
-                                        }
-                                    </div>
-
-                                    {wordData.definitions && 
-                                        <button
-                                        onClick={handleFavorite}
-                                        className="flex items-center uppercase font-gaegu text-lg tooltip bg-base-100 outline-0 focus:outline-0 focus-visible:outline-0 hover:border-0"
-                                        data-tip="Save your favorite words to your study set"
-                                        >
-                                            {isFavorite && <AiFillStar style={{ width: '40px', height: '40px', color: '#EBD678'}} />}
-                                            {!isFavorite && <AiOutlineStar style={{ width: '40px', height: '40px', color: '#EBD678'}} />}
-                                        </button>
-                                    }
-
-                                </div>
-                            }   
-
-                            {!wordData.definitions && 
-                                <figure className="mx-auto w-10/12">
-                                    <img 
-                                    src="../../wordNotFound.png"
-                                    alt="Error Message Picture"
-                                    />
-                                </figure>
+                            {wordData.definitions && 
+                                <button
+                                onClick={handleFavorite}
+                                className="flex items-center uppercase font-gaegu text-lg tooltip bg-base-100 outline-0 focus:outline-0 focus-visible:outline-0 hover:border-0"
+                                data-tip="Save your favorite words to your study set"
+                                >
+                                    {isFavorite && <AiFillStar style={{ width: '40px', height: '40px', color: '#EBD678'}} />}
+                                    {!isFavorite && <AiOutlineStar style={{ width: '40px', height: '40px', color: '#EBD678'}} />}
+                                </button>
                             }
 
-                            {wordData.definitions && wordData.definitions[0].image_url &&
-                            <figure className="mt-4 w-1/2 lg:w-2/5 lg:mt-0">
-                                <img 
-                                src={`${wordData.definitions[0].image_url}`} 
-                                alt={`Illustration of ${wordData.word}`}
-                                className="rounded-full border-dotted border-2 border-yellow-800 p-1"
-                                />
-                            </figure>
-                            }
                         </div>
+                    }   
 
-                        <CardBody wordData={wordData} />
+                    {!wordData.definitions && 
+                        <figure className="mx-auto w-10/12">
+                            <img 
+                            src="../../wordNotFound.png"
+                            alt="Error Message Picture"
+                            />
+                        </figure>
+                    }
 
-                    </div>
+                    {wordData.definitions && wordData.definitions[0].image_url &&
+                    <figure className="mt-4 w-1/2 lg:w-2/5 lg:mt-0">
+                        <img 
+                        src={`${wordData.definitions[0].image_url}`} 
+                        alt={`Illustration of ${wordData.word}`}
+                        className="rounded-full border-dotted border-2 border-yellow-800 p-1"
+                        />
+                    </figure>
+                    }
                 </div>
-                
+
+                <CardBody wordData={wordData} />
+
             </div>
     )
 }
